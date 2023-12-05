7656d28f6b8982bd910d7b43bd2ea560c72412c0
Solkakan <johanbrandell@gmail.com>
2023-12-05 22:55:23 +0100

    Solved Day 1 Part 1

--- Day1/day1p1.py
+++ Day1/day1p1.py
@@ -1,50 +1,50 @@
-
-
-
+from pathlib import Path
 
 
 def main():
-    with open('input.txt') as f:
+
+    data = Path(r'C:\Users\johan\AoC23\Day1\input.txt')
+
+    with open(data) as f:
         content = f.readlines()
 
-    data = []
+    total = 0
 
     for line in content:
-        out = []
-        for element in line:
-            match element:
-                case'1':
-                    out.append(1)
-                case'2':
-                    out.append(2)
-                case'3':
-                    out.append(3)
-                case'4':
-                    out.append(4)
-                case'5':
-                    out.append(5)
-                case'6':
-                    out.append(6)
-                case'7':
-                    out.append(7)
-                case'8':
-                    out.append(8)
-                case'9':
-                    out.append(9)
-                case'0':
-                    out.append(0)
+        total += linecalc(line)
 
-        data.append(out)
-
-    tot = 0
-
-    for element in data:
-        concat = [element[0], element[-1]]
-        print(''.join(str(item) for item in concat))
-
-           
+    print(total)
 
 
+def linecalc(line):
+    out = []
+    for element in line:
+        match element:
+            case "1":
+                out.append(1)
+            case "2":
+                out.append(2)
+            case "3":
+                out.append(3)
+            case "4":
+                out.append(4)
+            case "5":
+                out.append(5)
+            case "6":
+                out.append(6)
+            case "7":
+                out.append(7)
+            case "8":
+                out.append(8)
+            case "9":
+                out.append(9)
+            case "0":
+                out.append(0)
 
-if __name__ == '__main__':
+    nums = [out[0], out[-1]]
+    value = "".join(str(item) for item in nums)
+    return int(value)
+
+
+if __name__ == "__main__":
     main()
